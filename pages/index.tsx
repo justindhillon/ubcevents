48d392552742c59e90072b0803321a4939332b7b
justin.singh.dhillon <justin.singh.dhillon@gmail.com>
2023-10-07 15:01:42 -0700

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ export const getStaticProps: GetStaticProps = async () => {
   const feed = [
     {
       id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+      title: "First Post",
+      content: "This is just a test",
       published: false,
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
+        name: "Justin Dhillon",
+        email: "justin.singh.dhillon@gmail.com",
       },
     },
   ]
