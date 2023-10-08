c5c4d7601e59fe516bc4f654d91b464412c7180a
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-08 13:57:37 -0700

    modified:   pages/create.tsx

--- pages/create.tsx
+++ pages/create.tsx
@@ -5,14 +5,14 @@ import Router from "next/router";
 const Draft: React.FC = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [eventDate, setDate] = useState("");
+  const [eventDateString, setDate] = useState("");
 
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     try {
       // Need to turn date string into DateTime object
-      const usableDate = new Date(eventDate);
-      const body = { title, content, usableDate };
+      const eventDate = new Date(eventDateString);
+      const body = { title, content, eventDate };
       await fetch("/api/post", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -47,10 +47,10 @@ const Draft: React.FC = () => {
           <input 
             onChange={(e) => setDate(e.target.value)}
             type="date" 
-            value={eventDate}
+            value={eventDateString}
           />
           <br /><br />
-          <input disabled={!content || !title || !eventDate} type="submit" value="Create" />
+          <input disabled={!content || !title || !eventDateString} type="submit" value="Create" />
           <a className="back" href="#" onClick={() => Router.push("/")}>
             or Cancel
           </a>
