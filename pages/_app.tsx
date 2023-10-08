ac73a4dd302e8bb91310e5e523f147451f6b6138
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-08 09:50:34 -0700

    modified:   pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { Session } from "next-auth";
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps<{ session: Session; }>) => {
