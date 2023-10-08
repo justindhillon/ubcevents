24022eeaf318d07ab948662213a1d1270cb1012b
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-08 12:23:34 -0700

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>Public Feed</h1>
+        <h1>Upcoming Events</h1>
         <main>
           {props.feed.map((post) => (
             <div key={post.id} className="post">
