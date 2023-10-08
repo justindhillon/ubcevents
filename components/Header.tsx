a3ba6efb3e7bc9bfb9adddefdbad5aab8f218649
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-07 20:17:38 -0700

    modified:   components/Header.tsx 	modified:   pages/index.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
     <div className="left">
       <Link href="/">
         <a className="bold" data-active={isActive("/")}>
-          Feed
+          Events
         </a>
       </Link>
       <style jsx>{`
