54ea7a63417bf9eb80f99fbeae1818edf9b7654c
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-09 11:50:40 -0700

    modified:   scripts/deleteOldEvents.sh

--- scripts/deleteOldEvents.sh
+++ scripts/deleteOldEvents.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DATE_TODAY='date'
 
-sudo -i -u postgres psql -c "DELETE FROM Post WHERE eventDate <= $DATE_TODAY"
+sudo -i -u postgres psql -c "DELETE FROM Post WHERE eventDate::DATE <= $DATE_TODAY"
