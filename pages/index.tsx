68256851c19239ab9101f9be6903c7d34a3062ad
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-08 22:10:50 -0700

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,16 @@ type Props = {
 }
 
 const Blog: React.FC<Props> = (props) => {
+  // Sort by date string in ascending order
+  props.feed.sort(function(a, b) {
+    // Convert the date strings to Date objects
+    let dateA = new Date(a);
+    let dateB = new Date(b);
+
+    // Subtract the dates to get a value that is either negative, positive, or zero
+    return dateA - dateB;
+  });
+
   return (
     <Layout>
       <div className="page">
