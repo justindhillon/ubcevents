6a6dad3bddf451284f5186692c3db822d699e954
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-11 13:23:46 -0700

    modified:   pages/api/auth/[...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from 'next';
 import NextAuth from "next-auth";
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github'
 import prisma from '../../../lib/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
@@ -10,9 +10,9 @@ export default authHandler;
 const options = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   callbacks: {
