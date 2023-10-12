ad692c063a597fa110a07b84fdc6d82c06c030c8
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-12 09:59:40 -0700

    modified:   pages/about-me.tsx

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -23,17 +23,15 @@ const AboutMe: React.FC = () => {
                     />
                 </div>
                 <div className="column">
-                    <div>
-                        <p className="text-block">
-                            Welcome to my corner of the internet! 
-                            I'm Justin Dhillon, and I'm delighted that you've dropped by. 
-                            I’m a UBC computer science student with a deep love for solving problems. 
-                            I never knew what events were going on at UBC, so I made this! 
-                            ubcevents.com lets ubc students post all their events in one place! 
-                            ubcevents.com is made to give you the best university experience! 
-                            Keeping you in the loop, and on top of everything UBC!
-                        </p>
-                    </div>
+                    <p className="text-block">
+                        Welcome to my corner of the internet! 
+                        I'm Justin Dhillon, and I'm delighted that you've dropped by. 
+                        I’m a UBC computer science student with a deep love for solving problems. 
+                        I never knew what events were going on at UBC, so I made this! 
+                        ubcevents.com lets ubc students post all their events in one place! 
+                        ubcevents.com is made to give you the best university experience! 
+                        Keeping you in the loop, and on top of everything UBC!
+                    </p>
                 </div>
             </main>
             <div style={{textAlign:"center"}}>
@@ -52,7 +50,6 @@ const AboutMe: React.FC = () => {
             }
 
             .text-block {
-                text-align: left;
                 max-width: 500px;
             }
 
