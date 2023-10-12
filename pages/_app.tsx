a4641a3f869029f68c1b375e40f682c72475294d
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-11 20:24:24 -0700

    modified:   pages/_app.tsx 	deleted:    public/favicon.ico 	new file:   public/images/apple-touch-icon.png 	new file:   public/images/favicon-16x16.png 	new file:   public/images/favicon-32x32.png 	new file:   public/images/favicon.ico

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,10 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Head>
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
