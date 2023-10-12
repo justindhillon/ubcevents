2e1f8dfcab8dab8ae83b319dcf664c3890130251
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-12 13:50:20 -0700

    modified:   components/Post.tsx

--- components/Post.tsx
+++ components/Post.tsx
@@ -15,11 +15,15 @@ export type PostProps = {
 };
 
 const Post: React.FC<{ post: PostProps }> = ({ post }) => {
+  const dateObj = new Date(post.eventDate);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = dateObj.toLocaleDateString('en-US', options);
+
   return (
     <div onClick={() => Router.push("/p/[id]", `/p/${post.id}`)}>
       <h2>{post.title}</h2>
       <ReactMarkdown children={post.content} />
-      <p>📅 {post.eventDate}</p>
+      <p>📅 {formattedDate}</p>
       <p>⏰ {post.eventDate}</p>
       {post.location && <p>📍 {post.location}</p>}
       <style jsx>{`
