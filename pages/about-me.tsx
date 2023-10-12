7e03f73e6d84802d8e521888415df319ccb79a1b
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-12 12:09:56 -0700

    modified:   pages/about-me.tsx

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -125,6 +125,8 @@ const AboutMe: React.FC = () => {
                     display: grid;
                     grid-template-rows: repeat(3, 1fr);
                     grid-template-columns: repeat(2, 1fr);
+                    grid-row-gap: 1rem;
+                    text-alian: center;
                 }
 
                 @media screen and (max-width: 768px) {
