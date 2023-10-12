864bcc6d8fad407cb15da9384f9c97af31343d15
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-12 08:41:03 -0700

    modified:   components/Footer.tsx 	new file:   pages/about-me.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
     return (
         <div style={{textAlign:"center"}}>
             <br /><br />
-            <p>Made with <span style={{color:"#2596be"}}>♥</span> by <a href="https://github.com/justindhillon/ubcevents"><span style={{color:"#2596be"}}>Justin Dhillon</span></a></p>
+            <p>Made with <span style={{color:"#2596be"}}>♥</span> by <a href="https://ubcevents.com/about-me"><span style={{color:"#2596be"}}>Justin Dhillon</span></a></p>
             <br /><br />
         </div>
     )
