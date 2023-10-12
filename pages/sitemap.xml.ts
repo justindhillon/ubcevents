6daadce14d107be863082e92685356818998fb52
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-11 19:10:33 -0700

    modified:   pages/sitemap.xml.ts

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -4,8 +4,11 @@ const EXTERNAL_DATA_URL = 'https://ubcevents.com/p';
 
 function generateSiteMap(posts) {
     return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <!--We manually set the two URLs we know already-->
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
         <url>
             <loc>https://ubcevents.com/</loc>
         </url>
