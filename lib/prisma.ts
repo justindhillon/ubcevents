9dceb3f1792fd45b4bcb3106a066ad71aaac3c25
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-12 22:45:04 -0700

    modified:   lib/prisma.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.tsx 	deleted:    pages/api/get/index.ts 	modified:   pages/index.tsx

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,19 +3,15 @@ import { PrismaClient } from "@prisma/client";
 // Docs about instantiating `PrismaClient` with Next.js:
 // https://pris.ly/d/help/next-js-best-practices
 
-declare global {
-  var cachedPrisma: PrismaClient;
-}
-
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
-  prisma = global.cachedPrisma;
+  prisma = global.prisma;
 }
 
 export default prisma;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,18 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import Head from 'next/head';
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider session={pageProps.session}>
-        <Head>
-          <link rel="icon" href="/images/favicon/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
-        </Head>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </QueryClientProvider>
+    <SessionProvider session={pageProps.session}>
+      <Head>
+        <link rel="icon" href="/images/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+      </Head>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
