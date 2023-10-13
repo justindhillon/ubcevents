9736e92646f97b7746723e0c50a42a02e87936a1
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-13 16:00:52 -0700

    modified:   pages/test.tsx

--- pages/test.tsx
+++ pages/test.tsx
@@ -53,13 +53,13 @@ const Page = () => {
 			}
 		  </div>
 		))}
-		<button className="post" onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
-	      {isFetchingNextPage
-		    ? "Loading more..."
-		    : "Load More"
-		  }
-	    </button>
 	  </main>
+	  <button className="button" onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+	    {isFetchingNextPage
+		  ? "Loading more..."
+		  : "Load More"
+		}
+	  </button>
 	  <Footer />
 	</div>
 	<style jsx>{`
@@ -68,9 +68,46 @@ const Page = () => {
 		transition: box-shadow 0.1s ease-in;
 	  }
 
-	  .post button {
-		border: none;
-	  }
+.button {
+  align-items: center;
+  appearance: none;
+  background-color: #FCFCFD;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #36395A;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono",monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 18px;
+}
+
+.button:hover {
+  box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+  transform: translateY(-2px);
+}
+
+.button:active {
+  box-shadow: #D6D6E7 0 3px 7px inset;
+  transform: translateY(2px);
+}
 
 	  .post:hover {
 		box-shadow: 1px 1px 3px #aaa;
