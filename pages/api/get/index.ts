3848f59b17a7ba46d2eaddbf2267dd666e7a251a
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-12 21:45:43 -0700

    modified:   pages/api/get/index.ts

--- pages/api/get/index.ts
+++ pages/api/get/index.ts
@@ -1,7 +1,11 @@
 import prisma from "../../../lib/prisma";
 
 // GET /api/get
-export default async function GET(req: Request){
+// /app/api/users/route.ts
+
+import prisma from "@/lib/prisma";
+
+export async function GET(req: Request){
  try {
   // get page and lastCursor from query
   const url = new URL(req.url);
@@ -9,22 +13,16 @@ export default async function GET(req: Request){
   const take = url.searchParams.get("take");
   const lastCursor = url.searchParams.get("lastCursor");
 
-  let result = await prisma.post.findMany({
+  let result = await prisma.user.findMany({
     take: take ? parseInt(take as string) : 10,
     ...(lastCursor && {
-      skip: 1, // Do not include the cursor itself in the query result.
-      cursor: {
-        id: lastCursor as string,
-      }
+   skip: 1, // Do not include the cursor itself in the query result.
+   cursor: {
+     id: lastCursor as string,
+   }
     }),
-    where: { published: true, moderated: true },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
     orderBy: {
-      eventDate: "asc",
+   createdAt: "desc",
     }
   }
   );
@@ -32,36 +30,35 @@ export default async function GET(req: Request){
 
   if (result.length == 0) {
     return new Response(JSON.stringify({
-    data: [],
-    metaData: {
-      lastCursor: null,
-      hasNextPage: false,
-    },
-      }), { status: 200 });
+   data: [],
+   metaData: {
+     lastCursor: null,
+     hasNextPage: false,
+   },
+    }), { status: 200 });
   }
 
   const lastPostInResults: any = result[result.length - 1];
   const cursor: any = lastPostInResults.id;
 
-  const nextPage = await prisma.post.findMany({
+  const nextPage = await prisma.user.findMany({
     // Same as before, limit the number of events returned by this query.
     take: take ? parseInt(take as string) : 7,
     skip: 1, // Do not include the cursor itself in the query result.
     cursor: {
-      id: cursor,
+   id: cursor,
     },
   });
 
   const data = {
     data: result, metaData: {
-      lastCursor: cursor,
-      hasNextPage: nextPage.length > 0,
+   lastCursor: cursor,
+   hasNextPage: nextPage.length > 0,
     }
   };
 
   return new Response(JSON.stringify(data), { status: 200 });
-
-  } catch (error: any) {
-    return new Response(JSON.stringify(JSON.stringify({ error: error.message })), { status: 403 });
-  }
+   } catch (error: any) {
+  return new Response(JSON.stringify(JSON.stringify({ error: error.message })), { status: 403 });
+   }
 }
