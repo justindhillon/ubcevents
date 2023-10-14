6d27759f13e0c79fc064a56da594740104257e90
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-14 14:21:04 -0700

    modified:   pages/api/post/get.ts

--- pages/api/post/get.ts
+++ pages/api/post/get.ts
@@ -3,9 +3,9 @@ import prisma from "../../../lib/prisma";
 
 // POST /api/post/get
 export default async function handle(req, res) {
-  const page = req.body;
+  const page = req.body.body + 2;
   const feed = await prisma.post.findMany({
-		skip: (page.body - 1) * 5 + 1,
+		skip: (page.body - 1) * 5,
 		take: page.body * 5,
 		where: { published: true, moderated: true },
 		include: {
