44836daddee25263c091ef47ba9829fd7a38c657
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-03-19 16:37:50 -0700

    add portfolio website

--- pages/about-me/index.tsx
+++ pages/about-me/index.tsx
@@ -1,29 +1,13 @@
-import React from "react"
-import Layout from "../../components/Layout";
-import Head from 'next/head';
-import Intro from "./Intro";
-import Links from "./Links";
+import React, { useEffect } from "react";
 
 const AboutMe: React.FC = () => {
-    return (
-        <Layout>
-            <Head>
-                <title>UBC Events - About Me</title>
-                <meta property="description" content="About Me Page" />
-            </Head>
-            <div className="page">
-                <h1>About Me</h1>
-                <Intro />
-                <br /><br />
-                <Links />
-                <div style={{textAlign:"center"}}>
-                    <br /><br />
-                    <p>Back to <a href="/"><span style={{color:"#2596be"}}>Feed</span></a></p>
-                    <br /><br />
-                </div>
-            </div>
-        </Layout>
-    )
+    useEffect(() => {
+        // Redirect to the desired URL
+        window.location.href = "https://www.justin-dhillon.com/";
+    }, []);
+
+    // Render nothing since we're redirecting
+    return null;
 }
 
 export default AboutMe;
